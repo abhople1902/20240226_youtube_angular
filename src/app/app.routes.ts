0553557904281dcf812bbe8f76dd141e58dcb0b0
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2024-02-27 16:40:18 +0530

    settings page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import {Routes} from '@angular/router';
 import { MainBodyComponent } from './components/main-body/main-body.component';
 import { SubscriptionsContentComponent } from './components/subscriptions-content/subscriptions-content.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'subscriptions',
     component: SubscriptionsContentComponent
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent
   }
 ];
--- src/app/components/subscriptions-content/subscriptions-content.component.ts
+++ src/app/components/subscriptions-content/subscriptions-content.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-subscriptions-content',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './subscriptions-content.component.html',
   styleUrl: './subscriptions-content.component.css'
 })
 export class SubscriptionsContentComponent {
-
+  thumbnails = [
+    {
+      thumbnail1:"../../../assets/thumbnail1.jpg",
+      vidname:"F1 Singapore Qualifying Highlights by Ayush Bhople",
+      channel:"Ayush Bhople's creation",
+      
+    }
+  ]
 }
